d0b2439b2614717701bcf8101badff6a4dbacbad
Randal Vance Cunanan <randalvance@users.noreply.github.com>
2016-09-03 18:38:56 +0800

    28 support bb (#29)
    Resolves #28

---
+++ src/QuizMaster.BBCode/BBCodeParsingException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace QuizMaster.BBCode
+{
+    public class BBCodeParsingException : Exception
+    {
+        public BBCodeParsingException()
+        {
+        }
+        public BBCodeParsingException(string message)
+            : base(message)
+        {
+        }
+    }
+}
